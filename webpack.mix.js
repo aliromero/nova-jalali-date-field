f16517e2df6150834c626a1b32c5222518d50c9b
aliromero <aliyeketaz@gmail.com>
2023-09-29 03:17:43 +0330

    upp

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,4 +7,4 @@ mix
     .js('resources/js/field.js', 'js')
     .vue({version: 3})
     .css('resources/css/field.css', 'css')
-    .nova('romero/nova-persian-date-field')
+    .nova('aliromero/nova-jalali-date-field')
